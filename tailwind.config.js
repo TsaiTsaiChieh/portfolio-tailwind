cd91779cee1abc42f63a3e1ed0fcb67cbbf76955
Tsai-Chieh <d0177381@gmail.com>
2022-09-07 08:51:49 +0800

    :bug: to fix height problem if add <html> tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      height: {
+        'screen-1/2': '50vh'
+      },
       screens: {
         sm: "480px",
       },
       fontFamily: {
-
         ubuntu: ['Ubuntu', 'sans-serif']
       }
     },
