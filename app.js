f25dcc545b79be121ac554a5baf1fcf778d5d4b8
Tsai-Chieh <d0177381@gmail.com>
2022-09-11 20:25:28 +0800

    :sparkles: Tailwind Dark Mode Tutorial

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const body = document.querySelector('body')
 const hamburger = document.querySelector('#hamburger')
 const menu = document.querySelector('#menu')
 const hLinks = document.querySelectorAll('.hLink')
+const moon = document.querySelector('#moon')
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle('hidden')
@@ -24,3 +26,9 @@ hLinks.forEach(link => link.addEventListener('click', () => {
   menu.classList.toggle('hidden')
   hamburger.classList.toggle('bg-white')
 }))
+
+moon.addEventListener("click", () => {
+  console.log('ok');
+  body.classList.toggle('dark')
+
+})
